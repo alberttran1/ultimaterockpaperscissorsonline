4d8c7ce82ecbbfaae93eefc1155662c148adf12d
alberttran1 <albertq.tran1@gmail.com>
2025-07-31 15:58:44 -0400

    socket cors change

--- server/socket.js
+++ server/socket.js
@@ -12,7 +12,10 @@ const customRooms = new Map();
 export function setupSocket(server) {
   const io = new Server(server, {
     cors: {
-      origin: ['http://localhost:5173'],
+      origin: [
+        'http://localhost:5173',
+        'https://ultimaterockpaperscissorsonline.netlify.app',
+      ],
       credentials: true,
     },
   });
